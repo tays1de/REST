38f3a20732b57658669247455fea3c3a622fb45f
why_n0t_13@mail.ru <61447840+tays1de@users.noreply.github.com>
2021-10-02 14:53:54 +0300

    Add migration using Flyway;

---
+++ src/main/resources/db/migration/V1__InitDB_version.sql
@@ -0,0 +1,48 @@
+create table t_advertiser (
+    id  bigserial not null,
+    advertiser_name varchar(255),
+    phone_number varchar(255),
+    primary key (id));
+
+create table t_advertiser_company (
+        id  bigserial not null,
+        link_for_presentation varchar(255),
+        advertiser_id int8,
+        primary key (id));
+
+create table t_campaigns (
+    id  bigserial not null,
+    campaigns_name varchar(255),
+    language varchar(255),
+    link_on_photo varchar(255),
+    location varchar(255),
+    target_countries varchar(255),
+    advertiser_id int8,
+    primary key (id));
+
+
+create table t_role (
+    id int8 not null,
+    name varchar(255),
+    primary key (id));
+
+create table t_user (
+    id  bigserial not null,
+    password varchar(255),
+    username varchar(255),
+    primary key (id));
+
+create table t_user_roles (
+    user_id int8 not null,
+    roles_id int8 not null,
+    primary key (user_id, roles_id));
+
+
+alter table if exists t_advertiser_company
+        add constraint FKb7wck4lotjfw915aodi639kxb foreign key (advertiser_id) references t_advertiser;
+alter table if exists t_campaigns
+        add constraint FK3jmrpqidm7x6rhp0d8fo0kxxs foreign key (advertiser_id) references t_advertiser;
+alter table if exists t_user_roles
+        add constraint FKj47yp3hhtsoajht9793tbdrp4 foreign key (roles_id) references t_role;
+alter table if exists t_user_roles
+        add constraint FKpqntgokae5e703qb206xvfdk3 foreign key (user_id) references t_user;
---
+++ src/main/resources/db/migration/V2__Add_Admin.sql
@@ -0,0 +1,4 @@
+insert into t_role values (1,'ROLE_USER'),(2,'ROLE_ADMIN');
+
+insert into t_user values (0,'$2a$10$6/8QlucW0XEPTxrmtwTQvuq88R51KZQr4Od4JGF0nAFPrjf6WI0rW',,'admin'); --login: admin password: admin
+insert into t_user_roles values (0,1),(0,2);
