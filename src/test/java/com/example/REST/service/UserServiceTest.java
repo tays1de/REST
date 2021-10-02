a337f39ee3a59bc206c9f738e71da77cd73f29eb
why_n0t_13@mail.ru <61447840+tays1de@users.noreply.github.com>
2021-10-02 18:26:39 +0300

    Add unit-test;

--- src/test/java/com/example/REST/service/UserServiceTest.java
+++ src/test/java/com/example/REST/service/UserServiceTest.java
@@ -1,14 +1,45 @@
 package com.example.REST.service;
 
-
+import com.example.REST.entity.User;
+import com.example.REST.repository.UserRepository;
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Collection;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 class UserServiceTest {
 
+    @Autowired
+    private UserService userService;
+
+    @MockBean
+    private UserRepository userRepository;
+
+    @MockBean
+    private BCryptPasswordEncoder passwordEncoder;
 
 
     @Test
     void saveUser() {
+        User user = new User();
+        boolean result = userService.saveUser(user);
+        Assert.assertTrue(result);
 
+        Mockito.verify(userRepository,Mockito.times(1)).save(user);
     }
 }
